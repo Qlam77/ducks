8c9a3609dcd1b0d1d7b97ca1bf035c46a1c354ff
Quincy <qlam3@my.bcit.ca>
2018-01-24 23:50:29 -0800

    First Commit

---
+++ Lab3Assignment/Controllers/FilesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab3Assignment.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            string[] files = System.IO.Directory.GetFiles(System.Web.HttpContext.Current.Server.MapPath("~/TextFiles"));
+            for(int i = 0; i < files.Length; i++)
+            {
+                files[i] = System.IO.Path.GetFileNameWithoutExtension(files[i]);
+            }
+            ViewBag.files = files;
+            return View();
+        }
+
+        [Route("Display/{id?}"), ActionName("Display")]
+        public ActionResult Contents(string id)
+        {
+            if (id != null)
+            {
+                try
+                {
+                    string text = System.IO.File.ReadAllText(System.Web.HttpContext.Current.Server.MapPath("~/TextFiles/" + id + ".txt"));
+                    ViewBag.itemToShow = text;
+                }
+                catch(Exception E)
+                {
+                    ViewBag.itemToShow = "No Such File";
+                }
+            } else
+            {
+                ViewBag.itemToShow = "No Such File";
+            }
+            return View();
+        }
+    }
+}
---
+++ Lab3Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab3Assignment.MvcApplication" Language="C#" %>
